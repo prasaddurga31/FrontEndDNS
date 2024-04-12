07d09735d59e51aff8f7c2845ae595408d7385f6
prasaddurga31 <132526719+prasaddurga31@users.noreply.github.com>
2024-04-12 21:40:03 +0530

    Delete src directory

--- src/pages/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../style.css";
-export default function footer() {
-  return (
-    <>
-      <footer className="footer">All Right Reserved by DNS GURU.</footer>
-    </>
-  );
-}
